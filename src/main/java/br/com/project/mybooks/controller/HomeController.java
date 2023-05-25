b88db838d93955f5ff64ceac51b6a82c095bdc52
Duda Tomasio <dudasoarestomasio@gmail.com>
2023-05-25 20:27:56 -0300

    corrigindo repository

--- src/main/java/br/com/project/mybooks/controller/HomeController.java
+++ src/main/java/br/com/project/mybooks/controller/HomeController.java
@@ -1,5 +1,6 @@
 package br.com.project.mybooks.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-
     @GetMapping
     public String aplicacaoRodando(Model model) {
         return "home";
--- src/main/java/br/com/project/mybooks/repository/LivroRepository.java
+++ src/main/java/br/com/project/mybooks/repository/LivroRepository.java
@@ -3,5 +3,5 @@ package br.com.project.mybooks.repository;
 import br.com.project.mybooks.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LivroEntity extends JpaRepository<Livro, Long> {
+public interface LivroRepository extends JpaRepository<Livro, Long> {
 }
