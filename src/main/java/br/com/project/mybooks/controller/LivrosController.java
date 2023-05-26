519b9a9edbbf9a2f0f7eb73ab476f131c8f00f3b
Duda Tomasio <dudasoarestomasio@gmail.com>
2023-05-26 17:33:15 -0300

    criando formulario

---
+++ src/main/java/br/com/project/mybooks/controller/LivrosController.java
@@ -0,0 +1,16 @@
+package br.com.project.mybooks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/livros")
+public class LivrosController {
+
+    @GetMapping("/cadastro")
+    public String formulario() {
+
+        return "formulario";
+    }
+}
